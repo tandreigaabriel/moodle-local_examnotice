be9fbf40021f4a87102263be13618bbcb6a24df2
Toma Andrei G <tandreigaabriel@gmail.com>
2026-04-04 15:15:04 +0100

    update version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_examnotice';
-$plugin->version   = 2026032703;
-$plugin->requires  = 2024042200;
-$plugin->release   = '1.2.2';
+$plugin->version   = 2026040400; //
+$plugin->requires  = 2024042200; // Moodle 4.5+
+$plugin->release   = '1.0.0';    //
 $plugin->maturity  = MATURITY_STABLE;
